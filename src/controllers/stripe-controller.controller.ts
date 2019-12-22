af7993e655daa5826c920cc9452b0d3371507a1f
Gerhard Dinhof <g.dinhof@ibm.com>
2019-12-22 07:43:40 +0200

    refactoring; customer exist check

--- src/controllers/stripe-controller.controller.ts
+++ src/controllers/stripe-controller.controller.ts
@@ -14,51 +14,43 @@ export class StripeControllerController {
   @post('/members')
   signupMember() {
 
-    // TODO Check if customer already exists
-
-    // Create customer in Stripe
-    this.stripe.customers.create(
+    // Check if customer already exists
+    this.stripe.customers.list(
       {
-        name: 'Gerhard Dinhof',
         email: 'geahaad+005@gmail.com',
-        phone: '069910629413',
-        address: {
-          line1: 'Bahnhofstr 62-64/4/7, 3002 Purkersdorf',
-        },
+        limit: 1
       })
+      .then((customerArray: any) => {
+        if (customerArray.data.length > 0) {
+          const customerID = customerArray.data[0] ? customerArray.data[0].id : null;
+          console.debug(`Customer ${customerID} already exists`);
+          // Create new subscription in Stripe
+          this.createSubscription(customerID);
+        } else {
+          // Create new customer in Stripe
+          this.createCustomer();
+        }
+      })
+      .catch((err: any) => {
+        console.error(err);
+        throw (err);
+      });
+  }
+
+  private createCustomer() {
+    this.stripe.customers.create({
+      name: 'Gerhard Dinhof',
+      email: 'geahaad+005@gmail.com',
+      phone: '069910629413',
+      address: {
+        line1: 'Bahnhofstr 62-64/4/7, 3002 Purkersdorf',
+      },
+    })
       .then((customer: any) => {
         const customerID = customer.id;
         console.debug(`New customer ${customerID} created`);
-
         // Create subscription in Stripe
-        this.stripe.subscriptions.create(
-          {
-            customer: customerID,
-            items: [
-              {
-                plan: 'plan_GMfg76J8Cr4xb0'
-              }
-            ],
-            collection_method: "send_invoice",
-            days_until_due: 14,
-            // set billing cycle to start of upcoming year
-            billing_cycle_anchor: moment().add(1, 'year').startOf('year').unix(),
-          })
-          .then((subscription: any) => {
-            const subscriptionID = subscription.id;
-
-            console.debug(`New subscription ${subscriptionID} customer ${customerID} created`);
-
-            // Create subscription schedule in Stripe
-            // this.stripe.subscriptionSchedules.
-          })
-          .catch((err: any) => {
-            console.error(err);
-            return {
-              error: err,
-              status: 500
-            }
-          });
+        this.createSubscription(customerID);
       })
       .then((source: any) => {
         // return this.stripe.charges.create({
@@ -73,10 +65,32 @@ export class StripeControllerController {
       .catch((err: any) => {
         // Deal with an error
         console.error(err);
-        return {
-          error: err,
-          status: 500
+        throw (err);
+      });
+  }
+
+  private createSubscription(customerID: any) {
+    this.stripe.subscriptions.create({
+      customer: customerID,
+      items: [
+        {
+          plan: 'plan_GMfg76J8Cr4xb0'
         }
+      ],
+      collection_method: "send_invoice",
+      days_until_due: 14,
+      // set billing cycle to start of upcoming year
+      billing_cycle_anchor: moment().add(1, 'year').startOf('year').unix(),
+    })
+      .then((subscription: any) => {
+        const subscriptionID = subscription.id;
+        console.debug(`New subscription ${subscriptionID} customer ${customerID} created`);
+        // Create subscription schedule in Stripe
+        // this.stripe.subscriptionSchedules.
+      })
+      .catch((err: any) => {
+        console.error(err);
+        throw (err);
       });
   }
 }
