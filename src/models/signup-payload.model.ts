ad2127d9fc1ad89f925b8e091c4d32bdfb569dad
Gerhard Dinhof <g.dinhof@ibm.com>
2019-12-22 16:22:32 +0200

    aligned with changed payload structure

--- src/models/signup-payload.model.ts
+++ src/models/signup-payload.model.ts
@@ -3,31 +3,26 @@ import { Model, model, property } from '@loopback/repository';
 @model()
 export class SignupPayload extends Model {
   @property({
-    name: 'your-name',
     type: 'string',
   })
   name?: string;
 
   @property({
-    name: 'your-email',
     type: 'string',
   })
   email?: string;
 
   @property({
-    name: 'your-address',
     type: 'string',
   })
   address?: string;
 
   @property({
-    name: 'your-birthdate',
     type: 'string',
   })
   bod?: string;
 
   @property({
-    name: 'your-number',
     type: 'string',
   })
   phone?: string;
