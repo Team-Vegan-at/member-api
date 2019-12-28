19f12d8cb8b238763b11986de79739208e13e242
Gerhard Dinhof <g.dinhof@ibm.com>
2019-12-28 13:34:58 +0200

    express routing for /checkout/start

--- src/application.ts
+++ src/application.ts
@@ -1,17 +1,21 @@
-import {BootMixin} from '@loopback/boot';
-import {ApplicationConfig} from '@loopback/core';
+/* eslint-disable @typescript-eslint/no-floating-promises */
+import { BootMixin } from '@loopback/boot';
+import { ApplicationConfig } from '@loopback/core';
 import {
   RestExplorerBindings,
   RestExplorerComponent,
 } from '@loopback/rest-explorer';
-import {RestApplication} from '@loopback/rest';
-import {ServiceMixin} from '@loopback/service-proxy';
+import { RestApplication, OpenApiSpec } from '@loopback/rest';
+import { ServiceMixin } from '@loopback/service-proxy';
 import path from 'path';
-import {MySequence} from './sequence';
+import { MySequence } from './sequence';
+import express from 'express';
+import { Request, Response } from 'express';
 
 export class MemberApiApplication extends BootMixin(
   ServiceMixin(RestApplication),
 ) {
+
   constructor(options: ApplicationConfig = {}) {
     super(options);
 
@@ -20,6 +24,43 @@ export class MemberApiApplication extends BootMixin(
 
     // Set up default home page
     this.static('/', path.join(__dirname, '../public'));
+
+    // // Set up checkout page
+    const checkoutRouter = express();
+    checkoutRouter.get('/start', function (req: Request, res: Response) {
+      // ... Edit user UI ...
+      console.debug(`++ checkout()`);
+      // res.sendStatus(401);
+      res.redirect('/checkout/stripe');
+      // return res;
+    });
+
+    const spec: OpenApiSpec = {
+      openapi: '3.0.0',
+      info: {
+        title: 'LoopBack Application',
+        version: '1.0.0',
+      },
+      paths: {
+        '/checkout': {
+          get: {
+            parameters: [{ name: 'name', in: 'query', schema: { type: 'string' } }],
+            responses: {
+              '200': {
+                description: 'greeting text',
+                content: {
+                  'application/json': {
+                    schema: { type: 'string' },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    };
+
+    this.mountExpressRouter('/checkout', checkoutRouter, spec);
 
     // Customize @loopback/rest-explorer configuration here
     this.bind(RestExplorerBindings.CONFIG).to({
