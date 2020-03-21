06f23a8901eb87262a25296eefa7318fc6681059
Gerhard Dinhof <g.dinhof@ibm.com>
2020-03-21 09:05:32 +0200

    refactored cron into dedicated method

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-floating-promises */
-import {MemberApiApplication} from './application';
-import {ApplicationConfig} from '@loopback/core';
-import {DashboardController} from './controllers/dashboard.controller';
+import { MemberApiApplication } from './application';
+import { ApplicationConfig } from '@loopback/core';
+import { DashboardController } from './controllers/dashboard.controller';
 import moment from 'moment';
-import {RedisUtil} from './utils/redis.util';
-import {MollieController} from './controllers';
+import { RedisUtil } from './utils/redis.util';
+import { MollieController } from './controllers';
 
-export {MemberApiApplication};
+export { MemberApiApplication };
 
 export async function main(options: ApplicationConfig = {}) {
   require('dotenv').config();
@@ -24,67 +24,104 @@ export async function main(options: ApplicationConfig = {}) {
   debug(`Try ${url}/ping`);
 
   const CronJob = require('cron').CronJob;
-  const job = new CronJob('0 5 */1 * * *', async function() {
+  const job = new CronJob('0 5 */1 * * *', async function () {
     debugCron(`Cronjob start - ${moment().format()}`);
-    const dbc = new DashboardController();
-    await dbc.listDiscourseMembers();
-    await dbc.listMollieMembers();
-    await dbc.redisGetMollieCustomers().then((custList: any) => {
-      custList.forEach((custKey: string) => {
+
+    await cronProcessMembers(debugCron);
+
+    debugCron(`Cronjob finished - ${moment().format()}`);
+  });
+  job.start();
+
+  // Once off cron start
+  await cronProcessMembers(debugCron);
+
+  return app;
+}
+
+async function cronProcessMembers(debugCron: any) {
+  const dbc = new DashboardController();
+  await dbc.listDiscourseMembers();
+  await dbc.listMollieMembers();
+  await dbc.redisGetMollieCustomers().then((custList: any) => {
+    custList.forEach((custKey: string) => {
+      dbc
+        .redisGetMollieCustomer(custKey.replace(`${RedisUtil.mollieCustomerPrefix}:`, ''))
+        .then(async (custObj: any) => {
+          debugCron(custObj);
+          const mc = new MollieController();
+          const paymentObj = await mc.listCustomerPayments(custKey.replace(`${RedisUtil.mollieCustomerPrefix}:`, ''));
+          RedisUtil.redisGetAsync(`${RedisUtil.teamMemberPrefix}:${custObj.data.email.toLowerCase()}`)
+            .then((reply: any) => {
+              debugCron(`Redis returend ${reply}`);
+              if (reply == null) {
+                // Store in Redis
+                const redisMemberPayload = {
+                  name: custObj.data.name,
+                  email: custObj.data.email.toLowerCase(),
+                  mollieObj: custObj.data,
+                  molliePayments: paymentObj,
+                  discourseObj: null,
+                };
+                RedisUtil.redisClient.set(`${RedisUtil.teamMemberPrefix}:${custObj.data.email.toLowerCase()}`, JSON.stringify(redisMemberPayload), (err: any, _reply: any) => {
+                  if (err) {
+                    debugCron(`Redis error: ${err}`);
+                  }
+                });
+              }
+              else {
+                // Update in Redis
+                const updatePayload = JSON.parse(reply);
+                updatePayload.name = custObj.data.name;
+                updatePayload.mollieObj = custObj.data;
+                updatePayload.molliePayments = paymentObj;
+                RedisUtil.redisClient.set(`${RedisUtil.teamMemberPrefix}:${custObj.data.email.toLowerCase()}`, JSON.stringify(updatePayload), (err: any, _reply: any) => {
+                  if (err) {
+                    debugCron(`Redis error: ${err}`);
+                  }
+                });
+              }
+            })
+            .catch((err: any) => {
+              if (err) {
+                debugCron(`Redis error: ${err}`);
+              }
+            });
+        });
+    });
+    dbc.redisGetDiscourseCustomers().then((discourseList: any) => {
+      discourseList.forEach((custKey: string) => {
         dbc
-          .redisGetMollieCustomer(
-            custKey.replace(`${RedisUtil.mollieCustomerPrefix}:`, ''),
-          )
-          .then(async (custObj: any) => {
+          .redisGetDiscourseCustomer(custKey.replace(`${RedisUtil.discourseCustomerPrefix}:`, ''))
+          .then((custObj: any) => {
             debugCron(custObj);
-
-            const mc = new MollieController();
-            const paymentObj = await mc.listCustomerPayments(
-              custKey.replace(`${RedisUtil.mollieCustomerPrefix}:`, ''),
-            );
-
-            RedisUtil.redisGetAsync(
-              `${
-                RedisUtil.teamMemberPrefix
-              }:${custObj.data.email.toLowerCase()}`,
-            )
+            RedisUtil.redisGetAsync(`${RedisUtil.teamMemberPrefix}:${custObj.data.email.toLowerCase()}`)
               .then((reply: any) => {
                 debugCron(`Redis returend ${reply}`);
                 if (reply == null) {
                   // Store in Redis
                   const redisMemberPayload = {
                     name: custObj.data.name,
-                    email: custObj.data.email.toLowerCase(),
-                    mollieObj: custObj.data,
-                    molliePayments: paymentObj,
-                    discourseObj: null,
+                    email: custObj.data.email,
+                    mollieObj: null,
+                    molliePayments: null,
+                    discourseObj: custObj.data,
                   };
-                  RedisUtil.redisClient.set(
-                    `${
-                      RedisUtil.teamMemberPrefix
-                    }:${custObj.data.email.toLowerCase()}`,
-                    JSON.stringify(redisMemberPayload),
-                    (err: any, _reply: any) => {
-                      if (err) {
-                        debugCron(`Redis error: ${err}`);
-                      }
-                    },
-                  );
-                } else {
+                  RedisUtil.redisClient.set(`${RedisUtil.teamMemberPrefix}:${custObj.data.email.toLowerCase()}`, JSON.stringify(redisMemberPayload), (err: any, _reply: any) => {
+                    if (err) {
+                      debugCron(`Redis error: ${err}`);
+                    }
+                  });
+                }
+                else {
                   // Update in Redis
                   const updatePayload = JSON.parse(reply);
-                  updatePayload.molliePayments = paymentObj;
-                  RedisUtil.redisClient.set(
-                    `${
-                      RedisUtil.teamMemberPrefix
-                    }:${custObj.data.email.toLowerCase()}`,
-                    JSON.stringify(updatePayload),
-                    (err: any, _reply: any) => {
-                      if (err) {
-                        debugCron(`Redis error: ${err}`);
-                      }
-                    },
-                  );
+                  updatePayload.discourseObj = custObj.data;
+                  RedisUtil.redisClient.set(`${RedisUtil.teamMemberPrefix}:${custObj.data.email.toLowerCase()}`, JSON.stringify(updatePayload), (err: any, _reply: any) => {
+                    if (err) {
+                      debugCron(`Redis error: ${err}`);
+                    }
+                  });
                 }
               })
               .catch((err: any) => {
@@ -94,67 +131,6 @@ export async function main(options: ApplicationConfig = {}) {
               });
           });
       });
-
-      dbc.redisGetDiscourseCustomers().then((discourseList: any) => {
-        discourseList.forEach((custKey: string) => {
-          dbc
-            .redisGetDiscourseCustomer(
-              custKey.replace(`${RedisUtil.discourseCustomerPrefix}:`, ''),
-            )
-            .then((custObj: any) => {
-              debugCron(custObj);
-
-              RedisUtil.redisGetAsync(
-                `${RedisUtil.teamMemberPrefix}:${custObj.data.email}`,
-              )
-                .then((reply: any) => {
-                  debugCron(`Redis returend ${reply}`);
-                  if (reply == null) {
-                    // Store in Redis
-                    const redisMemberPayload = {
-                      name: custObj.data.name,
-                      email: custObj.data.email,
-                      mollieObj: null,
-                      molliePayments: null,
-                      discourseObj: custObj.data,
-                    };
-                    RedisUtil.redisClient.set(
-                      `${RedisUtil.teamMemberPrefix}:${custObj.data.email}`,
-                      JSON.stringify(redisMemberPayload),
-                      (err: any, _reply: any) => {
-                        if (err) {
-                          debugCron(`Redis error: ${err}`);
-                        }
-                      },
-                    );
-                  } else {
-                    // Update in Redis
-                    const updatePayload = JSON.parse(reply);
-                    updatePayload.discourseObj = custObj.data;
-                    RedisUtil.redisClient.set(
-                      `${RedisUtil.teamMemberPrefix}:${custObj.data.email}`,
-                      JSON.stringify(updatePayload),
-                      (err: any, _reply: any) => {
-                        if (err) {
-                          debugCron(`Redis error: ${err}`);
-                        }
-                      },
-                    );
-                  }
-                })
-                .catch((err: any) => {
-                  if (err) {
-                    debugCron(`Redis error: ${err}`);
-                  }
-                });
-            });
-        });
-      });
     });
-
-    debugCron(`Cronjob finished - ${moment().format()}`);
   });
-  job.start();
-
-  return app;
 }
