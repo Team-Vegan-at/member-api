62659fcec283cf619795d5b57cb1a34cb15d81e7
Gerhard Dinhof <g.dinhof@ibm.com>
2020-12-27 08:18:34 +0200

    create subscription, visual enhancements

--- public/membership/functions.js
+++ public/membership/functions.js
@@ -1,5 +1,18 @@
+/*** INIT ***/
 let baseUrl = 'http://localhost:3000';
+let searchParams = new URLSearchParams(window.location.search);
+let idHash;
+if (!searchParams.has('user')) {
+  console.error('User not found');
+  // TODO Error on site
+} else {
+  idHash = searchParams.get('user')
+}
 
+// Pre-fill e-mail field
+$('input[name="email"]').val(idHash);
+
+/*** FUNCTIONS ***/
 function fetchPayments(data) {
   let url = `${baseUrl}/membership/payments`;
   let searchParams = new URLSearchParams(window.location.search)
@@ -70,6 +83,52 @@ function cancelSubscription(callback) {
   });
 }
 
+function createSepaDD(callback) {
+  let url = `${baseUrl}/membership/mandate`;
+  let d = new Date();
+  let formattedDate = d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate();
+
+  // Step 1: Create Mandate
+  $.ajax({
+    url: `${url}?${$.param({
+        "email": idHash
+      })}`,
+    method: "POST",
+    contentType: "application/json",
+    data: JSON.stringify({
+      "name": $('input[name="name"]').val(),
+      "account": $('input[name="iban"]').val(),
+      "signDate": formattedDate,
+      "mandateRef": "TEAMVEGAN-" + Math.floor(100000 + Math.random() * 900000)
+    })
+  }).done(function () {
+    console.info("Mandate created successfully");
+    // Step 2: Create Subscription
+    url = `${baseUrl}/membership/subscription`;
+    $.ajax({
+      url: `${url}?${$.param({
+          "email": idHash
+        })}`,
+      method: "POST",
+    }).done(function () {
+      console.info("Subscription created successfully");
+      if (callback) {
+        callback();
+      }
+    }).fail(function (reason) {
+      console.error(reason.statusText);
+      if (callback) {
+        callback();
+      }
+    });
+  }).fail(function (reason) {
+    console.error(reason.statusText);
+    if (callback) {
+      callback();
+    }
+  });
+}
+
 function operateFormatter(value, row, index) {
   return [
     '<a class="remove btn btn-danger" href="javascript:void(0)" title="Remove">',
@@ -95,6 +154,7 @@ function loadingTemplate(message) {
   return '<div class="ph-item"><div class="ph-picture"></div></div>'
 }
 
+/*** EVENTS  ***/
 $('#subscriptions-table').bootstrapTable({
   formatNoMatches: function () {
     return 'Wir haben keinen Dauerauftrag gefunden';
@@ -106,3 +166,12 @@ $('#payments-table').bootstrapTable({
     return 'Wir haben keine Zahlungen gefundenn';
   }
 });
+
+$('#confirm-subscription').on('click', function() {
+  $('#loading').modal('show');
+
+  createSepaDD(function() {
+    $('#loading').modal('hide');
+  });
+});
+
